ac49ce784f2c45d138de618fd00e4a4b1f3d62c6
ks603 <kevin.shaughnessy18@gmail.com>
2020-04-30 23:15:36 -0400

    Created a laoder

--- game.js
+++ game.js
@@ -4,6 +4,8 @@ const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
 const progressBarFull = document.getElementById("progressBarFull");
+const loader = document.getElementById('loader')
+const game = document.getElementById('game')
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -54,6 +56,8 @@ startGame = () => {
   score = 0;
   availableQuesions = [...questions];
   getNewQuestion();
+  game.classList.remove('hidden')
+  loader.classList.add('hidden')
 };
 
 getNewQuestion = () => {
