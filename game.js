56f39b73703f1d93165815788636a34c3b95f006
ks603 <kevin.shaughnessy18@gmail.com>
2020-04-30 21:24:06 -0400

    CREATED HUD

--- game.js
+++ game.js
@@ -1,12 +1,13 @@
-const question = document.getElementById('question');
-const choices = Array.from(document.getElementsByClassName('choice-text'))
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById("questionCounter");
+const scoreText = document.getElementById("score");
 
-
-let currentQuestion = {}
-let acceptingAnswers = false
-let score = 0
-let questionCounter = 0
-let availableQuestions = []
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuesions = [];
 
 let questions = [
   {
@@ -36,59 +37,65 @@ let questions = [
   }
 ];
 
-// constants
-const CORRECT_BONUS = 10
-const MAX_QUESTIONS = 3
+//CONSTANTS
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
 
 startGame = () => {
-  questionCounter = 0 
-  score = 0
-  availableQuestions = [...questions]
-  getNewQuestion()
-}
+  questionCounter = 0;
+  score = 0;
+  availableQuesions = [...questions];
+  getNewQuestion();
+};
 
 getNewQuestion = () => {
+  if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    //go to the end page
+    return window.location.assign("/end.html");
+  }
+  questionCounter++;
+  questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
 
-    if(availableQuestions.length == 0 || questionCounter >= MAX_QUESTIONS) {
-      // Go to the end page
-      return window.location.assign('/end.html')
-    }
-  questionCounter++
-  const questionIndex = Math.floor(Math.random() * availableQuestions.length)
-    currentQuestion = availableQuestions[questionIndex]
-    question.innerText = currentQuestion.question
+  const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+  currentQuestion = availableQuesions[questionIndex];
+  question.innerText = currentQuestion.question;
 
-    choices.forEach( choice => {
-      const number = choice.dataset['number']
-      choice.innerText = currentQuestion['choice' + number]
-    })
+  choices.forEach(choice => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
 
-    availableQuestions.splice(questionIndex, 1)
-
-    acceptingAnswers = true
-}
+  availableQuesions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+};
 
 choices.forEach(choice => {
-  choice.addEventListener('click', e => {
-    if(!acceptingAnswers) return
+  choice.addEventListener("click", e => {
+    if (!acceptingAnswers) return;
 
-    acceptingAnswers = false
-    const selectedChoice = e.target
-    const selectedAnswer = selectedChoice.dataset['number']
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
 
-    const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
-    selectedChoice.parentElement.classList.add(classToApply)
-    
-    setTimeout( () => {
-      selectedChoice.parentElement.classList.remove(classToApply)
-      getNewQuestion()
-    }, 1000)
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
 
+    selectedChoice.parentElement.classList.add(classToApply);
 
-    
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
+});
 
-  })
-})
+incrementScore = num => {
+  score += num;
+  scoreText.innerText = score;
+};
 
-startGame()
+startGame();
